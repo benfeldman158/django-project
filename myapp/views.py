9008e48e5b22ab8b538a17010fb95e2cd07a8376
benfeldman158 <brfeldman1@gmail.com>
2025-02-09 18:48:44 -0500

    Added templates and updated views

--- myapp/views.py
+++ myapp/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Home Page!")
+    return render(request, 'myapp/home.html')
 
 def about(request):
-    return HttpResponse("This is the About Page!")
+    return render(request, 'myapp/about.html')
 
 def help_page(request):
     return render(request, 'help.html')  # Make sure you have this template
+
