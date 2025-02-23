5c5b6c864469edd635136d430820af99a6f60a46
benfeldman158 <brfeldman1@gmail.com>
2025-02-23 18:10:38 -0500

    Implemented Chat Room Messages CRUD

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,4 +11,9 @@ path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='l
     path('help/', views.help_page, name='help'),  # Help page
     path('restricted/', views.restricted_page, name='restricted'),
     path('register/', views.register, name='register'),
+
+     path('chat/', views.chat_room, name='chat_room'),
+    path('chat/create/', views.create_message, name='create_message'),
+    path('chat/update/<int:pk>/', views.update_message, name='update_message'),
+    path('chat/delete/<int:pk>/', views.delete_message, name='delete_message'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product  # Import your Product model
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import ChatMessage
+
+def chat_room(request):
+    messages = ChatMessage.objects.all().order_by('timestamp')
+    return render(request, 'myapp/chat_room.html', {'messages': messages})
+@login_required
+def create_message(request):
+    if request.method == "POST":
+        message_text = request.POST.get('message', '')
+        if message_text:
+            ChatMessage.objects.create(user=request.user, message=message_text)
+            return redirect('chat_room')
+    return render(request, 'myapp/create_message.html')
+@login_required
+def update_message(request, pk):
+    msg = get_object_or_404(ChatMessage, pk=pk, user=request.user)
+    if request.method == "POST":
+        new_message = request.POST.get('message', '')
+        if new_message:
+            msg.message = new_message
+            msg.save()
+            return redirect('chat_room')
+    return render(request, 'myapp/update_message.html', {'message': msg})
+@login_required
+def delete_message(request, pk):
+    msg = get_object_or_404(ChatMessage, pk=pk, user=request.user)
+    if request.method == "POST":
+        msg.delete()
+        return redirect('chat_room')
+    return render(request, 'myapp/delete_message.html', {'message': msg})
 
 def search(request):
     query = request.GET.get('q', '')  # Get the search query from the URL
